cad77a3cd616367f677bd5623f4ad9d723736104
Alexander Simmerl <alx@soundcloud.com>
2012-01-14 21:51:11 +0100

    Extracts response helper into own file

--- test/system/apps-test.sh
+++ test/system/apps-test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env roundup
 
-describe "Webreduce/Apps"
-
+# helper
+source test/helper/response.sh
 req="./test/helper/request"
+
+describe "Webreduce/Apps"
 
 before() {
   app_name="nyan"
@@ -10,18 +12,14 @@ before() {
 
 it_creates_app() {
   resp=$($req -u "http://localhost:5000/apps/$app_name" -v "PUT")
-  code=$(extract_code $resp)
+  code=$(status_code $resp)
 
   test $code = "201";
 }
 
 it_updates_app() {
   resp=$($req -u "http://localhost:5000/apps/$app_name" -v "PUT")
-  code=$(extract_code $resp)
+  code=$(status_code $resp)
 
   test $code = 204;
-}
-
-extract_code() {
-  echo $2
 }
