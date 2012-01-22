42dac1e342aa1c2b1448315bfca45983174139e3
David T. Lehmann <dtl@voxelbrain.com>
2012-01-22 17:19:55 +0100

    static rule with HEAD injection

---
+++ src/pkg/webreduce/router/router.go
@@ -0,0 +1,54 @@
+package router
+
+import (
+	"regexp"
+)
+
+// the regular expression to extract variable names from rule specs
+var ruleRegex *regexp.Regexp = regexp.MustCompile("<[a-z][a-zA-Z]+>")
+
+// parses a rule spec in the form /foo/<bar> where 'bar' is the name of a
+// variable. It returns a regex.Regexp for extraction of variables and
+// the list of names for those variables
+func parseRuleSpec(spec string) (regex *regexp.Regexp, variables []string) {
+	return
+}
+
+// Rule is the representation of a URL rule
+type Rule struct {
+	spec string
+	methods []string
+	regex *regexp.Regexp
+	variables []string
+}
+
+// Create a new Rule. The rule spec ...
+// If 'GET' is provided as methods, 'HEAD' is automaticaly added
+// to the new rule.
+func NewRule(spec string, methods ...string) *Rule {
+	regex, variables := parseRuleSpec(spec)
+
+	verbs := map[string]bool{}
+	ms := []string{}
+
+	if len(methods) != 0 {
+		for _, method := range methods {
+			switch method {
+			case "GET":
+				verbs["HEAD"] = true
+				verbs["GET"] = true
+			default:
+				verbs[method] = true
+			}
+		}
+
+		for method, _ := range verbs {
+			ms = append(ms, method)
+		}
+
+	} else {
+		ms = []string{"HEAD", "GET"}
+	}
+
+	return &Rule{spec, ms, regex, variables}
+}
---
+++ src/pkg/webreduce/router/router_test.go
@@ -0,0 +1,50 @@
+package router
+
+import (
+	"testing"
+)
+
+func TestStaticRuleSpec(t *testing.T) {
+	spec := "/test"
+	rule := NewRule(spec)
+
+	if rule.spec != spec {
+		t.Errorf("Got Rule.spec %v, expected %v", rule.spec, spec)
+	}
+}
+
+
+func TestStaticRuleHeadInjection(t *testing.T) {
+	spec := "/test"
+	rule := NewRule(spec)
+
+	if l := len(rule.methods); l != 2 {
+		t.Errorf("Got Rule.methods of length %v, expected 2", l)
+	}
+
+	methods := map[string]bool{"GET": true, "HEAD": true}
+
+	for _, method := range rule.methods {
+		if _, found := methods[method]; found != true {
+			t.Errorf("Got unexpected method '%v'", method)
+		}
+	}
+}
+
+func TestStaticRuleWithMethods(t *testing.T) {
+	spec := "/test"
+	rule := NewRule(spec, "GET", "PUT", "POST", "DELETE")
+	methods := map[string]bool{
+		"HEAD": true,
+		"GET": true,
+		"PUT": true,
+		"POST": true,
+		"DELETE": true,
+	}
+
+	for _, method := range rule.methods {
+		if _, found := methods[method]; found != true {
+			t.Errorf("Got unexpected method '%v'", method)
+		}
+	}
+}
