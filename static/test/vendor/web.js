c6bcfe7d3a553b27135979f2f4c1075dad33d911
Alexander Simmerl <alx@soundcloud.com>
2012-01-30 00:51:48 +0100

    Remove ender dependency

--- static/test/vendor/web.js
+++ static/test/vendor/web.js
@@ -1,36 +1,13 @@
-/*
- * Copyright (c) 2011 Anton Kovalyov, http://hirojs.com/
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+/*global hiro:false, jQuery: false, $:false */
 
-/*global hiro:false, ender:false */
-
-(function (window, undefined) {
+(function ($, undefined) {
 	"use strict";
 
 	var document = window.document;
 	var context  = '#web';
 
 	hiro.bind('hiro.onComplete', function () {
-		ender(document.createElement('p'))
+		$(document.createElement('p'))
 			.addClass('simple')
 			.html('All tests finished')
 			.appendTo(context);
@@ -40,22 +17,22 @@
 		var uid = 'hiro_suite_' + suite.name;
 		var div = document.createElement('div');
 
-		ender(div)
+		$(div)
 			.addClass('suite')
 			.addClass('idle')
 			.attr('id', uid);
 
-		ender(document.createElement('h2'))
+		$(document.createElement('h2'))
 			.html(suite.name)
 			.appendTo(div);
 
-		ender(context).append(div);
+		$(context).append(div);
 
 		context = '#' + uid;
 	});
 
 	hiro.bind('suite.onComplete', function (suite, success) {
-		ender(context)
+		$(context)
 			.removeClass('idle')
 			.addClass(success ? 'succ' : 'fail');
 
@@ -63,7 +40,7 @@
 	});
 
 	hiro.bind('suite.onTimeout', function (suite, success) {
-		ender(context)
+		$(context)
 			.removeClass('idle')
 			.addClass('fail');
 
@@ -74,59 +51,59 @@
 		var uid = 'hiro_test_' + test.suite.name + '_' + test.name;
 		var div = document.createElement('div');
 
-		ender(div)
+		$(div)
 			.addClass('test')
 			.addClass('idle')
 			.attr('id', uid)
 			.html(test.name);
 
-		ender(document.createElement('div'))
+		$(document.createElement('div'))
 			.addClass('report')
 			.hide()
 			.appendTo(div);
 
-		ender(div).appendTo(context);
+		$(div).appendTo(context);
 		context = '#' + uid;
 	});
 
 	hiro.bind('test.onFailure', function (test, report) {
-		var div = ender('div.report', context);
+		var div = $('div.report', context);
 
-		ender(document.createElement('p'))
+		$(document.createElement('p'))
 			.html('<label>Assertion:</label> ' + report.assertion)
 			.appendTo(div);
 
 		if (report.expected) {
-			ender(document.createElement('p'))
+			$(document.createElement('p'))
 				.html('<label>Expected:</label> ' + report.expected)
 				.appendTo(div);
 		}
 
-		ender(document.createElement('p'))
+		$(document.createElement('p'))
 			.html('<label>Result:</label> ' + report.result)
 			.appendTo(div);
 
-		ender(document.createElement('p'))
+		$(document.createElement('p'))
 			.html('<label>Position:</label> ' + report.position)
 			.appendTo(div);
 	});
 
 	hiro.bind('test.onComplete', function (test, success) {
-		ender(context)
+		$(context)
 			.removeClass('idle')
 			.addClass(success ? 'succ' : 'fail');
 
 		if (!success)
-			ender('div.report', context).show();
+			$('div.report', context).show();
 
 		context = '#hiro_suite_' + test.suite.name;
 	});
 
 	hiro.bind('test.onTimeout', function (test, success) {
-		ender(context)
+		$(context)
 			.removeClass('idle')
 			.addClass('fail');
 
 		context = '#hiro_suite_' + test.suite.name;
 	});
-}(window));
+}(jQuery, window));
