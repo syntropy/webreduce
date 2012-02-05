580b1c4b05268646f9d247096789fdf9d5cb5efd
Alexander Simmerl <alx@soundcloud.com>
2012-02-05 05:44:04 +0100

    Test get static file

--- static/test/api/app.js
+++ static/test/api/app.js
@@ -1,8 +1,9 @@
 hiro.module('APP', {
   onTest: function() {
     var app = { name: 'coma' };
+    var html = '<html><head><title>COMA</title></head><body></body></html>';
 
-    this.args = [ app ];
+    this.args = [ app, html ];
   },
   "test PUT": function(app) {
     var h = this;
@@ -19,7 +20,7 @@ hiro.module('APP', {
       h.resume();
     });
   },
-  "test PUT static html": function(app) {
+  "test PUT static html": function(app, html) {
     var h = this;
 
     h.expect(1);
@@ -27,9 +28,24 @@ hiro.module('APP', {
 
     PUT({
       url: '/' + app.name + '/index.html',
-      data: '<html><head><title>COMA</title></head><body></body></html>'
+      data: html
     }, function(res) {
       h.assertEqual(res.status, 204);
+
+      h.resume();
+    });
+  },
+  "test GET static html": function(app, html) {
+    var h = this;
+
+    h.expect(2);
+    h.pause();
+
+    GET({
+      url: '/' + app.name + '/index.html',
+    }, function(res) {
+      h.assertEqual(res.status, 200);
+      h.assertEqual(res.body, html)
 
       h.resume();
     });
