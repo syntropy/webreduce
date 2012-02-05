92fc601bf3a6be38170448cd30f9b19786614dc7
David T. Lehmann <dtl@voxelbrain.com>
2012-02-05 04:43:52 +0100

    replacing old wrd cmd with wrdnxt

--- cmd/wrd/main.go
+++ cmd/wrd/main.go
@@ -2,36 +2,31 @@ package main
 
 import (
 	"net/http"
-	"wr"
+	"wr/web/app"
 	"wr/web/app/sensor"
 	"wr/web/router"
 )
 
 func main() {
-	cfg := &wr.StringContext{}
-	data := map[string]string{
-		"db/url":             "localhost",
-		"db/name":            "webreduce",
-		"db/collection/name": "sensors",
-	}
-
-	for k, v := range data {
-		cfg.Set(k, v)
-	}
-
-	sensor, err := sensor.NewSensorCollectionApi(cfg)
+	dburl := ""
+	apps, err := app.NewApi(dburl, "apps")
 	if err != nil {
 		panic(err)
 	}
-	defer sensor.Close()
+	defer apps.Close()
 
-	r := router.NewRouter("/sensors")
-	r.AddRoute("", func(ctx wr.Context, w http.ResponseWriter, r *http.Request) { sensor.GetList(ctx, w, r) }, "GET")
-	r.AddRoute("/<sensor>", func(ctx wr.Context, w http.ResponseWriter, r *http.Request) { sensor.GetSensor(ctx, w, r) }, "GET")
-	r.AddRoute("/<sensor>", func(ctx wr.Context, w http.ResponseWriter, r *http.Request) { sensor.PutSensor(ctx, w, r) }, "PUT")
-	r.AddRoute("/<sensor>", func(ctx wr.Context, w http.ResponseWriter, r *http.Request) { sensor.PostToSensor(ctx, w, r) }, "POST")
+	r := router.NewRouter("/<app>")
+	apps.RegisterRoutes(&r)
+
+	sensors, err := sensor.NewApi(dburl, "apps", "sensors")
+	if err != nil {
+		panic(err)
+	}
+	defer apps.Close()
+
+	sensors.RegisterRoutes(&r)
 
 	http.Handle("/", &r)
-	http.Handle("/test/", http.StripPrefix("/test/", http.FileServer(http.Dir("./static/test"))))
+	// http.Handle("/test/", http.StripPrefix("/test/", http.FileServer(http.Dir("./static/test"))))
 	http.ListenAndServe(":8080", nil)
 }
